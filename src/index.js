b279e8d5c3db8c5ee1901163cfa09fd500b67322
wlnnsrr14 <wsari2974@gmail.com>
2025-06-10 19:44:07 +0700

    Initial commit of my PWA app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; //default dari CRA
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
